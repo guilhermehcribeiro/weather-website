916a1b7535c66bfa7bc8c7bfb659689d3c87a572
guilherme-ribeiro-wd <43786735+guilherme-ribeiro-wd@users.noreply.github.com>
2019-11-14 13:47:27 -0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.getElementById('msg-1')
+const messageTwo = document.getElementById('msg-2')
+
+weatherForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    const location = search.value
+
+    fetch('/weather?address=' + location)
+    .then(response => {
+        response.json().then(data => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
